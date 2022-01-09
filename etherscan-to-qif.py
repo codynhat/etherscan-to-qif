50e6068f11de07453582a3c2e766ff547cde0ee1
Cody Hatfield <cody.hatfield@me.com>
2022-01-09 13:20:46 -0800

    Simple export

---
+++ etherscan-to-qif.py
@@ -0,0 +1,31 @@
+import qifparse.qif as qif
+import datetime
+import csv
+import os
+
+ADDRESS=os.environ['ADDRESS']
+
+with open('transactions.csv', 'r') as csv_file:
+    reader = csv.reader(csv_file)
+
+    qif_obj = qif.Qif()
+
+    acc = qif.Account(name='ETH Wallet', account_type='Invst')
+    qif_obj.add_account(acc)
+
+    for row in reader:
+        if row[4] != ADDRESS:
+            continue
+
+        txhash = row[0]
+        timestamp = datetime.datetime.fromtimestamp(int(row[2]))
+        txnFeeETH = float(row[10])
+        price = float(row[12])
+
+        tr1 = qif.Investment(date=timestamp, action="SellX", quantity=txnFeeETH, price=price, memo=txhash, security="ETH")
+
+        tr1._fields[4].custom_print_format='%s%.18f'
+
+        acc.add_transaction(tr1, header='!Type:Invst')
+
+    print(str(qif_obj))
