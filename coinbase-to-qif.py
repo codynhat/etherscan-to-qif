5df294163278718ae4b6c6f5c3fff4c93cd2a031
Cody Hatfield <cody.hatfield@me.com>
2022-02-09 15:55:38 -0800

    Add Coinbase Convert

--- coinbase-to-qif.py
+++ coinbase-to-qif.py
@@ -2,6 +2,7 @@ import qifparse.qif as qif
 import datetime
 import csv
 import os
+import re
 
 ADDRESS=os.environ['ADDRESS']
 
@@ -39,6 +40,24 @@ with open('coinbase.csv', 'r') as csv_file:
             tr1._fields[4].custom_print_format='%s%.18f'
 
             acc.add_transaction(tr1, header='!Type:Invst')
+        elif row[1] == "Convert":
+            sellAmount = float(row[3])
+            sellCurrency = row[2]
+            spotPrice = float(row[5])
+
+            match = re.findall(r".+ (.+) (.+)$", memo)
+            buyAmount = float(match[0][0])
+            buyCurrency = match[0][1]
+            buyPrice = (spotPrice * sellAmount) / buyAmount
+
+            tr1 = qif.Investment(date=timestamp, action="Sell", quantity=sellAmount, price=spotPrice, memo=memo, security=(sellCurrency+'-USD'))
+            tr2 = qif.Investment(date=timestamp, action="Buy", quantity=buyAmount, price=buyPrice, memo=memo, security=(buyCurrency+'-USD'))
+
+            tr1._fields[4].custom_print_format='%s%.10f'
+            tr2._fields[4].custom_print_format='%s%.10f'
+
+            acc.add_transaction(tr1, header='!Type:Invst')
+            acc.add_transaction(tr2, header='!Type:Invst')
 
 
     print(str(qif_obj))
